3b88292f9dda586dc4bd02c4d5275b92d8bb6372
Ylli <yllibllaca@gmail.com>
2022-12-18 22:32:37 +0100

    updated_branch_push

---
+++ apps/home.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+
+def app():
+    st.title("Home")
+
+    st.markdown(
+        """
+    A [streamlit](https://streamlit.io) app template for geospatial applications based on [streamlit-option-menu](https://github.com/victoryhb/streamlit-option-menu). 
+    To create a direct link to a pre-selected menu, add `?page=<app name>` to the URL, e.g., `?page=upload`.
+    https://share.streamlit.io/giswqs/streamlit-template?page=upload
+
+    """
+    )
+
+    m = leafmap.Map(location = [42.569264,20.901489], zoom_start=8)
+    # m.add_geojson('data/kufiri_ks.geojson')
+    # m.fit_bounds(m.get_bounds(), padding=(30, 30))
+    # m.zoom_to_bounds([[[18.775635,41.619549],[18.775635,43.34116],[23.554688,43.34116],[23.554688,41.619549],[18.775635,41.619549]]])
+    # m.add_basemap("ROADMAP")
+    m.to_streamlit(height=700)
---
+++ streamlit_app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from apps import home, heatmap, upload  # import your app modules here
+
+st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
+
+# A dictionary of apps in the format of {"App title": "App icon"}
+# More icons can be found here: https://icons.getbootstrap.com
+
+apps = [
+    {"func": home.app, "title": "Home", "icon": "house"},
+    # {"func": heatmap.app, "title": "Heatmap", "icon": "map"},
+    {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+]
+
+titles = [app["title"] for app in apps]
+titles_lower = [title.lower() for title in titles]
+icons = [app["icon"] for app in apps]
+
+params = st.experimental_get_query_params()
+
+if "page" in params:
+    default_index = int(titles_lower.index(params["page"][0].lower()))
+else:
+    default_index = 0
+
+with st.sidebar:
+    selected = option_menu(
+        "Main Menu",
+        options=titles,
+        icons=icons,
+        menu_icon="cast",
+        default_index=default_index,
+    )
+
+    st.sidebar.title("About")
+    st.sidebar.info(
+        'EZ - Geodesy Interactive Web App'
+
+
+    
+    )
+
+for app in apps:
+    if app["title"] == selected:
+        app["func"]()
+        break
