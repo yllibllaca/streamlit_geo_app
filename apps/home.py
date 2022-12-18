7f68635aae44723f4f67c83f92828e7be044cfb5
Ylli <yllibllaca@gmail.com>
2022-12-18 22:34:18 +0100

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -14,7 +14,7 @@ def app():
     """
     )
 
-    m = leafmap.Map(location = [42.569264,20.901489], zoom_start=8)
+    m = leafmap.Map(location = [42.569264,20.901489], zoom_start=12)
     # m.add_geojson('data/kufiri_ks.geojson')
     # m.fit_bounds(m.get_bounds(), padding=(30, 30))
     # m.zoom_to_bounds([[[18.775635,41.619549],[18.775635,43.34116],[23.554688,43.34116],[23.554688,41.619549],[18.775635,41.619549]]])
